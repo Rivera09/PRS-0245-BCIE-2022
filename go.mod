346e9ff2d7e3ac1fc1e2ee190bad22b17ffeddb1
= <=>
2022-12-03 21:39:26 -0600

    :tada: initial commit

---
+++ go.mod
@@ -0,0 +1,8 @@
+module PRS-0245-BCIE-2022
+
+go 1.19
+
+require (
+	github.com/jaswdr/faker v1.15.0 // indirect
+	github.com/rabbitmq/amqp091-go v1.5.0 // indirect
+)
---
+++ producer.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
+
+type ProccesedClients struct {
+	Day  int
+	Hour int
+}
+}
+
+func main() {
+	/*var daysRunning, stationsAmount, resourcesAmount int
+
+	// TODO: Add validtion for inputs
+	fmt.Println("Ingrese el número de días que desea ejecutar la simulación")
+	fmt.Scanln(&daysRunning)
+
+	fmt.Println("Ingrese la cantidad de estaciones a utilizar")
+	fmt.Scanln(&stationsAmount)
+
+	fmt.Println("Ingrese la cantidad de recursos a utilizar")
+	fmt.Scanln(&resourcesAmount)*/
+	
+
+
+
+		/*var daysConvertedToMins = daysRunning * 1400
+		var passedDays = 0*/
+
+	/*for i := 0; i < daysConvertedToMins; i++ {
+		if i%1400 == 0 {
+			passedDays += 1
+		}
+	}*/
+}
+
+
